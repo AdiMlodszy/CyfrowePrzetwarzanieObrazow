9ef10374a1291810565d204a61d12493ea1e8823
addktiv <adriankasprzakschool@gmail.com>
2025-03-31 21:43:15 +0200

    CHORE: New branch and config for collab version of project

--- src/train_yolo.py
+++ src/train_yolo.py
@@ -1,15 +1,20 @@
-# scripts/train_yolo.py
-
 from ultralytics import YOLO
+from google.colab import drive
 
-model = YOLO("yolov8n.pt")
+drive.mount('/content/drive')
+
+model = YOLO("/content/CyfrowePrzetwarzanieObrazow/runs/detect-train/weights/best.pt")  
 
 model.train(
-    data="/app/data/dataset/data.yaml",
-    epochs=50,
-    imgsz=640,
-    batch=16,
-    project="runs",
-    name="detect-train",
-    exist_ok=True
+    data="/content/CyfrowePrzetwarzanieObrazow/data/dataset/data.yaml",  
+    epochs=10,  # Number of epochs to train
+    batch=4, # Batch size
+    imgsz=640, # Image size for training
+    resume=True,  # Resume training from the last checkpoint
+    name="resume_training",  # Name of the training run
+    exist_ok=True,  # Allow overwriting existing runs
+    project="/content/drive/MyDrive/CyfrowePrzetwarzanieObrazow/runs/detect",  # Directory to save the training results
+    cache=False,  # Cache images for faster training
+    workers=2,  # Number of workers for data loading
+    rect=False,  # Rectangular training
 )
