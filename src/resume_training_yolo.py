b402126ef8b279cf66a85e85b7588e48e9419748
addktiv <adriankasprzakschool@gmail.com>
2025-04-30 20:01:26 +0200

    FEAT: Update script for resuming YOLO training

--- src/resume_training_yolo.py
+++ src/resume_training_yolo.py
@@ -1,18 +1,33 @@
-from ultralytics import YOLO
+import os, tempfile, sys
 
-model = YOLO("/app/runs/detect-train/weights/best.pt")  
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from scripts.load_config import load_config
+from ultralytics import YOLO
+from pathlib import Path
+
+CONFIG_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'config.yaml'))
+
+# 0.  tryb środowiska
+mode     = os.getenv("ENV_MODE", "local")      # 'local' domyślnie
+
+# 1.  config
+cfg      = load_config(CONFIG_PATH)
+paths    = cfg[f"{mode}_paths"]
+
+# 2.  model
+
+raw_yaml= Path(paths["dataset_yaml"]).read_text()
+filled  = os.path.expandvars(raw_yaml)
+
+tmp_yaml = Path(tempfile.gettempdir()) / "dataset.yaml"
+tmp_yaml.write_text(filled)
+
+# 3.  trening
+model   = YOLO(paths["weights"])
 
 model.train(
-    data="/app/data/data.yaml",  
-    epochs=10,  # Number of epochs to train
-    batch=8, # Batch size
-    imgsz=640, # Image size for training
-    resume=True,  # Resume training from the last checkpoint
-    name="resume_training",  # Name of the training run
-    exist_ok=True,  # Allow overwriting existing runs
-    project="/app/runs/detect",  # Directory to save the training results
-    save_period=1,  # Save model every epoch
-    cache=True,  # Cache images for faster training
-    workers=2,  # Number of workers for data loading
-    rect=True,  # Rectangular training
+    data    = str(tmp_yaml),
+    project = paths["output_dir"],
+    **cfg["training"]
 )
